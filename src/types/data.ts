1f86f9b29891be602e708136eade54fb00f8c49b
Zura Jijavadze <mailzura@gmail.com>
2024-04-18 23:46:07 +0400

    pretify files

--- src/types/data.ts
+++ src/types/data.ts
@@ -1,37 +1,37 @@
 export interface TranslationResponse {
-    from: string;
-    to: string;
-    str: string;
-    found: Translation[] | [];
+  from: string
+  to: string
+  str: string
+  found: Translation[] | []
 }
 
 export interface Translation {
-    id: number;
-    ka: string;
-    en: string;
-    de: string;
-    ka_description: string;
-    en_description: string;
-    de_description: string;
-    source_id: number;
-    created_at: string;
-    updated_at: string;
-    source: TranslationSource;
+  id: number
+  ka: string
+  en: string
+  de: string
+  ka_description: string
+  en_description: string
+  de_description: string
+  source_id: number
+  created_at: string
+  updated_at: string
+  source: TranslationSource
 }
 
 export interface TranslationSource {
-    id: number;
-    ka: string;
-    en: string;
-    de: string;
-    status: number;
-    created_at: string;
-    updated_at: string;
+  id: number
+  ka: string
+  en: string
+  de: string
+  status: number
+  created_at: string
+  updated_at: string
 }
 
 export interface TranslationItem {
-    id: number;
-    ka: string;
-    en: string;
-    source: string;
+  id: number
+  ka: string
+  en: string
+  source: string
 }
